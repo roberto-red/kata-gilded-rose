1bace2a263b5a8967a50645a4ca402ae633dfff8
Fran Bosquet <fran@redradix.com>
2018-06-25 14:58:06 +0200

    Add update brie helper function

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,11 +1,11 @@
 import Item from "./Item";
 
-const VEST = new Item("+5 Dexterity Vest", 10, 20);
-const BRIE = new Item("Aged Brie", 2, 0);
-const ELIXIR = new Item("Elixir of the Mongoose", 5, 7);
-const SULFURAS = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-const PASS = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-const CAKE = new Item("Conjured Mana Cake", 3, 6);
+let VEST = new Item("+5 Dexterity Vest", 10, 20);
+let BRIE = new Item("Aged Brie", 2, 0);
+let ELIXIR = new Item("Elixir of the Mongoose", 5, 7);
+let SULFURAS = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+let PASS = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+let CAKE = new Item("Conjured Mana Cake", 3, 6);
 
 const makeNameChecker = name => item => item.name == name;
 
@@ -25,22 +25,23 @@ const sellInUnderSix = item => item.sellIn < 6;
 const sellInUnderZero = item => item.sellIn < 0;
 const sellInLessOrEqualsZero = item => item.sellIn <= 0;
 
-const updateSulfuras = item => ({
-  quality: 80,
-  sellIn: item.sellIn,
-  name: item.name
-});
+const updateSulfuras = sulfuras => sulfuras;
 
-const GildedRose = function() {
-  var items = [];
-  items.push(VEST);
-  items.push(BRIE);
-  items.push(ELIXIR);
-  items.push(SULFURAS);
-  items.push(PASS);
-  items.push(CAKE);
-  GildedRose.updateQuality(items);
+const updateBrie = brie => {
+  brie.quality++;
+  brie.sellIn--;
+
+  sellInUnderEleven(brie) && brie.quality++;
+  sellInUnderSix(brie) && brie.quality++;
+
+  sellInUnderZero(brie) && (brie.quality = 0);
+
+  qualityOverFifty(brie) && (brie.quality = 50);
+
+  return brie;
 };
+
+const GildedRose = function() {};
 
 GildedRose.updateQuality = function(items) {
   for (var i = 0; i < items.length; i++) {
@@ -51,7 +52,14 @@ GildedRose.updateQuality = function(items) {
       continue;
     }
 
-    if (isNotBrie(item) && isNotPass(item)) {
+    if (isBrie(item)) {
+      item = updateBrie(item);
+      continue;
+    }
+
+    //
+
+    if (isNotPass(item)) {
       if (qualityOverZero(item)) {
         item.quality--;
       }
@@ -66,21 +74,15 @@ GildedRose.updateQuality = function(items) {
     item.sellIn--;
 
     if (sellInUnderZero(item)) {
-      if (isNotBrie(item)) {
-        if (isNotPass(item)) {
-          if (qualityOverZero(item)) {
-            item.quality--;
-          }
-        } else {
-          item.quality = 0;
+      if (isNotPass(item)) {
+        if (qualityOverZero(item)) {
+          item.quality--;
         }
       } else {
-        if (qualityUnderFifty(item)) {
-          item.quality++;
-        }
-        if (isBrie(item) && sellInLessOrEqualsZero(item)) item.quality = 0;
+        item.quality = 0;
       }
     }
+
     if (qualityOverFifty(item)) item.quality = 50;
   }
   return items;
