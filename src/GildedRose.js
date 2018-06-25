8119b6a48b1ca296b8b64921bc69ef415eacdb5f
Fran Bosquet <fran@redradix.com>
2018-06-25 15:25:12 +0200

    Clean, make data inmutable, make it functional

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,17 +1,8 @@
 import Item from "./Item";
 
-let VEST = new Item("+5 Dexterity Vest", 10, 20);
 let BRIE = new Item("Aged Brie", 2, 0);
-let ELIXIR = new Item("Elixir of the Mongoose", 5, 7);
 let SULFURAS = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
 let PASS = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-let CAKE = new Item("Conjured Mana Cake", 3, 6);
-
-const makeNameChecker = name => item => item.name == name;
-
-const isBrie = makeNameChecker(BRIE.name);
-const isSulfuras = makeNameChecker(SULFURAS.name);
-const isPass = makeNameChecker(PASS.name);
 
 const qualityOverFifty = item => item.quality > 50;
 
@@ -19,55 +10,42 @@ const sellInUnderEleven = item => item.sellIn < 11;
 const sellInUnderSix = item => item.sellIn < 6;
 const sellInUnderZero = item => item.sellIn < 0;
 
-const updateSulfuras = sulfuras => sulfuras;
+const updateSulfuras = sulfuras => ({ ...sulfuras });
 
-const updateAged = brie => {
-  brie.quality++;
-  brie.sellIn--;
+const updateAged = item => {
+  const aged = { ...item };
+  aged.quality++;
+  aged.sellIn--;
 
-  sellInUnderEleven(brie) && brie.quality++;
-  sellInUnderSix(brie) && brie.quality++;
+  sellInUnderEleven(aged) && aged.quality++;
+  sellInUnderSix(aged) && aged.quality++;
 
-  sellInUnderZero(brie) && (brie.quality = 0);
+  sellInUnderZero(aged) && (aged.quality = 0);
 
-  qualityOverFifty(brie) && (brie.quality = 50);
-
-  return brie;
+  qualityOverFifty(aged) && (aged.quality = 50);
+  return aged;
 };
 
 const updateRegular = item => {
-  item.quality--;
-  item.sellIn--;
+  const regular = { ...item };
+  regular.quality--;
+  regular.sellIn--;
 
-  sellInUnderZero(item) && item.quality--;
+  sellInUnderZero(regular) && regular.quality--;
+  return regular;
 };
+
+const getUpdater = item =>
+  ({
+    [SULFURAS.name]: updateSulfuras,
+    [BRIE.name]: updateAged,
+    [PASS.name]: updateAged
+  }[item.name] || updateRegular);
+
+const update = item => getUpdater(item)(item);
 
 const GildedRose = function() {};
 
-GildedRose.updateQuality = function(items) {
-  for (var i = 0; i < items.length; i++) {
-    let item = items[i];
-
-    if (isSulfuras(item)) {
-      item = updateSulfuras(item);
-      continue;
-    }
-
-    if (isBrie(item)) {
-      item = updateAged(item);
-      continue;
-    }
-
-    if (isPass(item)) {
-      item = updateAged(item);
-      continue;
-    }
-    //
-
-    item = updateRegular(item);
-    continue;
-  }
-  return items;
-};
+GildedRose.updateQuality = items => items.map(update);
 
 export default GildedRose;
