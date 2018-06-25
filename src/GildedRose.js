9978a7d132258786e2bc283da94522854e3ad508
Victor Miguez <victor.miguez.martinez@gmail.com>
2018-06-25 10:42:56 +0200

    :art: extract conditions to query functions, simplify ifs structure, kind of group by product

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -11,72 +11,74 @@ const GildedRose = function () {
   GildedRose.updateQuality(items)
 }
 
+function isAgedBrie(item) {
+  return "Aged Brie" == item.name
+}
+
+function isNotAgedBrie(item) {
+  return !isAgedBrie(item)
+}
+
+function isBackstage(item) {
+  return "Backstage passes to a TAFKAL80ETC concert" == item.name
+}
+
+function isNotBackstage(item) {
+  return !isBackstage(item)
+}
+
+function isNotSulfuras(item) {
+  return "Sulfuras, Hand of Ragnaros" != item.name
+}
+
 GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
+    const item = items[i]
+
+    if (isNotSulfuras(item)) {
+
+      item.sellIn = item.sellIn - 1
+
+      if (isNotAgedBrie(item) && isNotBackstage(item)) {
+        item.quality = item.quality - 1
+        if (item.sellIn < 0) {
+          item.quality = item.quality - 1
         }
       }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
+
+      if (isAgedBrie(item)) {
+        item.quality = item.quality + 1
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 1
         }
-        //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
+        if (item.sellIn < 6) {
+          item.quality = item.quality + 1
         }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            // See revision number 2394 on SVN.
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
+        if (item.sellIn < 0) {
+          item.quality = 0
         }
       }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality
+
+      if (isBackstage(item)) {
+        item.quality = item.quality + 1
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 1
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (item.sellIn < 6) {
+          item.quality = item.quality + 1
         }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0
-      } // of for.
+        if (item.sellIn < 0) {
+          item.quality = 0
+        }
+      }
+
+      if (item.quality > 50) {
+        item.quality = 50
+      }
     }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-      if (items[i].quality > 50) items[i].quality = 50
+
   }
   return items
-};
+}
 
 export default GildedRose
