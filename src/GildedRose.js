9e80f29a11f8e2f9ade3555e4f11a32ae05b229a
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-02 17:33:43 +0200

    Refactor GildedRose.updateQuality
    Original code had too much cyclomatic complexity. Luckily, there were tests in place.

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,5 +1,54 @@
 import Item from './Item'
 
+const LEGENDARY_ITEM_NAMES = [
+  "Sulfuras, Hand of Ragnaros",
+]
+
+const PERISHABLE_ITEM_NAMES = [
+  "Aged Brie",
+  "Backstage passes to a TAFKAL80ETC concert",
+]
+
+const WELL_AGING_ITEM_NAMES = [
+  "Aged Brie",
+  "Backstage passes to a TAFKAL80ETC concert",
+]
+
+
+const calculateWellAgingQualityIncrease = ({ sellIn, quality }) => {
+  if (sellIn <= 5) return 3
+  if (sellIn <= 10) return 2
+  return 1
+}
+
+const calculateWellAgingUpdatedQuality = (item) => {
+  const { quality } = item
+  const proposedQuality = quality + calculateWellAgingQualityIncrease(item)
+
+  if (proposedQuality > 50) return 50
+  return proposedQuality
+}
+
+const calculateUpdatedSellIn = (item) => {
+  const { name, sellIn } = item
+
+  if (LEGENDARY_ITEM_NAMES.includes(name)) return sellIn
+
+  return sellIn - 1
+}
+
+const calculateUpdatedQuality = (item) => {
+  const { name, sellIn, quality } = item
+
+  if (LEGENDARY_ITEM_NAMES.includes(name)) return quality
+  if (PERISHABLE_ITEM_NAMES.includes(name) && sellIn <= 0) return 0
+  if (WELL_AGING_ITEM_NAMES.includes(name)) return calculateWellAgingUpdatedQuality(item)
+
+  if (sellIn <= 0) return quality - 2
+  return quality - 1
+}
+
+
 const GildedRose = function () {
   var items = []
   items.push(new Item("+5 Dexterity Vest", 10, 20))
@@ -12,71 +61,11 @@ const GildedRose = function () {
 }
 
 GildedRose.updateQuality = function (items) {
-  for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            // See revision number 2394 on SVN.
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0
-      } // of for.
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-      if (items[i].quality > 50) items[i].quality = 50
-  }
-  return items
+  return items.map((item) => ({
+    ...item,
+    quality: calculateUpdatedQuality(item),
+    sellIn: calculateUpdatedSellIn(item),
+  }))
 };
 
 export default GildedRose
