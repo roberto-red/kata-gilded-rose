a83a8d9fadfbb2d8628baa0817878d025ebbe618
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-03 13:26:17 +0200

    Construct items in place instead of by pushes

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -59,13 +59,14 @@ const calculateUpdatedQuality = (item) => {
 
 
 const GildedRose = function () {
-  var items = []
-  items.push(new Item("+5 Dexterity Vest", 10, 20))
-  items.push(new Item("Aged Brie", 2, 0))
-  items.push(new Item("Elixir of the Mongoose", 5, 7))
-  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
-  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
-  items.push(new Item("Conjured Mana Cake", 3, 6))
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20),
+    new Item("Aged Brie", 2, 0),
+    new Item("Elixir of the Mongoose", 5, 7),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Conjured Mana Cake", 3, 6),
+  ]
   GildedRose.updateItems(items)
 }
 
