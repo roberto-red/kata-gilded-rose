e44706fcba3d07e5d5d6f30d7f8cfcfa9667ff43
Victor Miguez <victor@redradix.com>
2020-09-08 12:19:12 +0200

    extract common handler for well aging products

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -15,52 +15,36 @@ const GildedRose = function () {
   // GildedRose.updateQuality(items)
 }
 
+const wellAgingHandler = item => {
+  item.sellIn = item.sellIn - 1
+
+  if (item.quality < 50) {
+    item.quality = item.quality + 1
+
+    if (item.sellIn <= 5) {
+      item.quality = item.quality + 1
+    }
+
+    if (item.sellIn <= 10) {
+      item.quality = item.quality + 1
+    }
+  }
+
+  if (item.sellIn < 0) {
+    item.quality = 0
+  }
+
+  if (item.quality > 50) {
+    item.quality = 50
+  }
+}
+
 const PRODUCT_HANDLERS = {
   [SULFURAS]: item => {
     item.quality = 80
   },
-  [AGED_BRIE]: item => {
-    item.sellIn = item.sellIn - 1
-  
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-
-      if (item.sellIn <= 5) {
-        item.quality = item.quality + 1
-      }
-
-      if (item.sellIn <= 10) {
-        item.quality = item.quality + 1
-      }
-    }
-    if (item.sellIn < 0) {
-      item.quality = 0
-    }
-    if (item.quality > 50) {
-      item.quality = 50
-    }
-  },
-  [BACKSTAGE]: item => {
-    item.sellIn = item.sellIn - 1
-    
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-
-      if (item.sellIn <= 10) {
-        item.quality = item.quality + 1
-      }
-
-      if (item.sellIn <= 5) {
-        item.quality = item.quality + 1
-      }
-    }
-    if (item.sellIn < 0) {
-      item.quality = item.quality - item.quality
-    }
-    if (item.quality > 50) {
-      item.quality = 50
-    }
-  }
+  [AGED_BRIE]: wellAgingHandler,
+  [BACKSTAGE]: wellAgingHandler
 }
 
 const defaultHandler = item => {
